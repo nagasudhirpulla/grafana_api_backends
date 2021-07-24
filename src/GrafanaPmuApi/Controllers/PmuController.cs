7a331811a5d4cb8ae8865357fc51e30abf95af02
unknown <nagasudhirpulla@gmail.com>
2021-07-24 21:59:43 +0530

    started pmu historian api

---
+++ src/GrafanaPmuApi/Controllers/PmuController.cs
@@ -0,0 +1,96 @@
+﻿using GrafanaCore.Models.Query;
+using GrafanaCore.Models.Response;
+using GrafanaCore.Models.Tags;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PmuHistorianUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrafanaPmuApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PmuController : ControllerBase
+    {
+        private readonly PmuHistFetcher _pmuHistFetcher;
+        public PmuController(PmuHistFetcher pmuHistFetcher)
+        {
+            _pmuHistFetcher = pmuHistFetcher;
+        }
+
+        [HttpGet]
+        public IActionResult Get() => Ok();
+
+        [HttpPost("query")]
+        public IActionResult Query([FromBody] QueryRequest query)
+        {
+            // initialise response
+            List<TargetResponse> dataResponse = new();
+
+            // iterate through each series for data fetch
+            foreach (QueryTarget trgt in query.Targets)
+            {
+                string samplingType = trgt.Target;
+
+                // derive extra data object
+                var dataObj = trgt.ExtraData;
+
+                // derive series name
+                string pntName = dataObj.PntName ?? dataObj.Pnt ?? trgt.Target;
+
+                // derive fetch shift
+                TimeSpan fetchShift = new(days: dataObj.FetchShiftDays, hours: dataObj.FetchShiftHrs, minutes: dataObj.FetchShiftMins, seconds: dataObj.FetchShiftSecs);
+
+                // fetch data
+                var measData = _pmuHistFetcher.FetchHistData(dataObj.Pnt, query.Range.From, query.Range.To, samplingType, dataObj.SamplingPeriod, fetchShift, dataObj.FetchFuture);
+
+                // add data to response
+                dataResponse.Add(new TargetResponse(pntName, measData));
+            }
+
+            // create the json response string
+            var jsonResult = JsonConvert.SerializeObject(dataResponse);
+
+            // return the response with 200 status header
+            return Ok(jsonResult);
+        }
+
+        [HttpPost("search")]
+        public IActionResult Search()
+        {
+            return Ok(new List<string>() { "raw", "snap", "average", "max", "min" });
+        }
+
+        //[HttpPost("tag-keys")]
+        //public IActionResult TagKeys()
+        //{
+        //    var jsonResult = JsonConvert.SerializeObject(new List<TagKeyResponse>() { new TagKeyResponse("string", "sampling") });
+        //    return Ok(jsonResult);
+        //}
+
+        //[HttpPost("tag-values")]
+        //public IActionResult TagValues([FromBody] TagValueRequest query)
+        //{
+        //    List<TagValueResponse> tagVals = new();
+        //    if (query.Key == "sampling")
+        //    {
+        //        tagVals = new List<TagValueResponse>() {
+        //            new TagValueResponse("raw"),
+        //            new TagValueResponse("snap"),
+        //            new TagValueResponse("average"),
+        //            new TagValueResponse("max"),
+        //            new TagValueResponse("min"),
+        //            new TagValueResponse("interpolated")
+        //        };
+        //    }
+        //    var jsonResult = JsonConvert.SerializeObject(tagVals);
+        //    return Ok(jsonResult);
+        //}
+
+    }
+}
---
+++ src/PmuHistorianUtils/PmuHistFetcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PmuHistorianUtils
+{
+    public class PmuHistFetcher
+    {
+        private static readonly DateTime Epoch = new(1970, 1, 1, 0, 0, 0, 0);
+        private readonly ILogger<PmuHistFetcher> _logger;
+        private readonly bool _useRandom;
+
+        public PmuHistFetcher(ILogger<PmuHistFetcher> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _useRandom = configuration.GetValue<bool>("UseRandom");
+        }
+
+        private static List<List<double>> FetchRandomHistData(DateTime startTime, DateTime endTime, int samplingPeriod, bool isFetchFuture)
+        {
+            List<List<double>> reslt = new();
+            DateTime curTime = startTime;
+            int resFreq = (samplingPeriod > 0) ? samplingPeriod : 60;
+            Random rand = new();
+            DateTime targetEndTime = endTime;
+            if (!isFetchFuture && targetEndTime > DateTime.UtcNow)
+            {
+                targetEndTime = DateTime.UtcNow;
+            }
+            while (curTime <= targetEndTime)
+            {
+                reslt.Add(new List<double>() { rand.Next(50, 100), curTime.Subtract(Epoch).TotalMilliseconds });
+                curTime += TimeSpan.FromSeconds(resFreq);
+            }
+            return reslt;
+        }
+
+        public List<List<double>> FetchHistData(string pnt, DateTime startTime, DateTime endTime, string type, int samplingPeriod, TimeSpan fetchShift, bool isFetchFuture)
+        {
+            return FetchRandomHistData(startTime, endTime, samplingPeriod, isFetchFuture);
+        }
+    }
+}
